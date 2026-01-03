90ceb1b798bab1a6ed043d4ed71ebe357a95c3f6
Jason Boykin <jasonboykin@gmail.com>
2026-01-03 16:16:59 -0600

    disable auth temporarily - will revisit later

--- apps/dashboard/middleware.ts
+++ apps/dashboard/middleware.ts
@@ -1,9 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from 'next/server'
 
-export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/sign-up", "/sign-in/(.*)", "/sign-up/(.*)"],
-});
+export function middleware() {
+  return NextResponse.next()
+}
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+}
